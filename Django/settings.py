6ccb21442c075ded730f9ea25287c925b39fafc7
Thuy Dang Tran <thuydt@greenglobal.vn>
2019-12-19 23:12:55 +0700

    first page django

--- Django/settings.py
+++ Django/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pages.apps.PagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,8 +55,7 @@ ROOT_URLCONF = 'Django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates/pages')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
